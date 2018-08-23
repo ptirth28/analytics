1f9d9553cf6007e7a06bb1b3a77f04f39c620500
tirthpatel28898 <tirthpatel28898@gmail.com>
2018-08-23 23:36:06 +0530

    a

---
+++ newspop.R
@@ -0,0 +1,14 @@
+#dbms
+
+df=read.csv('dbms.csv')
+df
+names(df)
+table(df$shares)
+tabulate(df$shares)
+mean(df$shares)
+dim(df)
+o1 =aggregate(cbind(weekday_is_monday==1 & weekday_is_tuesday==1 & weekday_is_wednesday==1)~url,data = df,FUN = mean)
+names(df)
+write.csv(o1,'o1.csv')
+o2 = df[weekday_is_monday==1 & weekday_is_tuesday==1 & weekday_is_wednesday==1,]
+o2
